8e2ec2e2c742772390115472663a49a50eeb38d9
taranu <dan.s.taranu@gmail.com>
2016-06-08 15:59:41 +0800

    Deprecated; use makeSersic with nser=0.5 or makeGaussianPS instead.

--- R/profitGenPSF.R
+++
@@ -1,9 +0,0 @@
-profitGenPSF=function(FWHM=3,sigma,npix=25){
-  if(missing(sigma)){sigma=FWHM/(2*sqrt(2*log(2)))}
-  x0=y0=npix/2+0.5
-  y=matrix(1:npix,npix,npix)
-  x=t(y)
-  psfanal=exp(-(((x - x0)^2/(2 * sigma^2)) + ((y - y0)^2/(2 * sigma^2))))
-  psfanal=psfanal/sum(psfanal)
-  return(psfanal)
-}
