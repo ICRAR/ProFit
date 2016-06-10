9d7a71bb719c2de082c55941aaccd00fc77b30c7
Aaron Robotham <aaron.robotham@uwa.edu.au>
2016-06-10 16:41:16 +0800

    I think I've fixed things up so the checks run with only 1 Note (size of package, which is not an issue).

--- R/profitMisc.R
+++ R/profitMisc.R
@@ -2,9 +2,3 @@ profitCheckFinesample <- function(finesample)
 {
   stopifnot(is.integer(finesample) && finesample >= 1L)
 }
-
-# FWHM = factor x sigma = Re(n=0.5)
-profitFWHMFactor <- function()
-{
-  return(2.3548200450309493)
-}
