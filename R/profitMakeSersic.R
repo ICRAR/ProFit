c994bbeafe2e9ef3497da8e7bdfdc8ea52dcf7bd
Rodrigo Tobar <rtobar@icrar.org>
2016-06-24 12:23:48 +0800

    Removing extra parameters from profitMakeSersic
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- R/profitMakeSersic.R
+++ R/profitMakeSersic.R
@@ -1,4 +1,4 @@
-profitMakeSersic <- function(CALCREGION, XCEN = 0, YCEN = 0, MAG = 15, RE = 1, NSER = 1, ANG = 0, AXRAT = 1, BOX = 0, MAGZERO = 0, ROUGH = FALSE, XLIM = as.numeric( c(-100,100)), YLIM = as.numeric( c(-100,100)), DIM = as.integer( c(200,200)), UPSCALE = 9L, MAXDEPTH = 2L, RESWITCH = 1, ACC = 0.1, DOCALCREGION = FALSE) {
+profitMakeSersic <- function(CALCREGION, XCEN = 0, YCEN = 0, MAG = 15, RE = 1, NSER = 1, ANG = 0, AXRAT = 1, BOX = 0, MAGZERO = 0, ROUGH = FALSE, XLIM = as.numeric( c(-100,100)), YLIM = as.numeric( c(-100,100)), DIM = as.integer( c(200,200)), ACC = 0.1, DOCALCREGION = FALSE) {
 	# Simply prepare a model with one sersic profile and fire
 	model = list(
 		sersic = list(
@@ -11,5 +11,5 @@ profitMakeSersic <- function(CALCREGION, XCEN = 0, YCEN = 0, MAG = 15, RE = 1, N
 			axrat=AXRAT,
 			box=BOX)
 	)
-	return (profitMakeModel(model, magzero=MAGZERO, dim=DIM, rough=ROUGH, maxdepth=MAXDEPTH, reswitch=RESWITCH, acc=ACC, calcregion=CALCREGION, docalcregion=DOCALCREGION))
+	return (profitMakeModel(model, magzero=MAGZERO, dim=DIM, rough=ROUGH, acc=ACC, calcregion=CALCREGION, docalcregion=DOCALCREGION))
 }
