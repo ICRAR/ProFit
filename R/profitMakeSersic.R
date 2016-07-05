043748b782b52421c2940d4fec92494b0604f72e
Aaron Robotham <aaron.robotham@uwa.edu.au>
2016-07-05 14:26:07 +1000

    Got rid of capitalisation for profitMakeSersic.

--- R/profitMakeSersic.R
+++ R/profitMakeSersic.R
@@ -1,17 +1,15 @@
-profitMakeSersic <- function(XCEN = 0, YCEN = 0, MAG = 15, RE = 1, NSER = 1, ANG = 0, AXRAT = 1, BOX = 0, MAGZERO = 0, ROUGH = FALSE, XLIM = as.numeric( c(-100,100)), YLIM = as.numeric( c(-100,100)), DIM = as.integer( c(200,200)), ACC = 0.1, DOCALCREGION = FALSE, CALCREGION) {
+profitMakeSersic <- function(xcen = 0, ycen = 0, mag = 15, re = 1, nser = 1, ang = 0, axrat = 1, box = 0, magzero = 0, rough = FALSE, xlim = as.numeric( c(-100,100)), ylim = as.numeric( c(-100,100)), dim = as.integer( c(200,200)), acc = 0.1, docalcregion = FALSE, calcregion) {
 	# Simply prepare a model with one sersic profile and fire
 	model = list(
 		sersic = list(
-			xcen = XCEN+(XLIM[2]-XLIM[1])/2,
-			ycen=YCEN+(YLIM[2]-YLIM[1])/2,
-			mag=MAG,
-			re=RE,
-			nser=NSER,
-			ang=ANG,
-			axrat=AXRAT,
-			box=BOX)
+			xcen = xcen+(xlim[2]-ylim[1])/2,
+			ycen=ycen+(xlim[2]-ylim[1])/2,
+			mag=mag,
+			re=re,
+			nser=nser,
+			ang=ang,
+			axrat=axrat,
+			box=box)
 	)
-	return (profitMakeModel(model, magzero=MAGZERO, dim=DIM, rough=ROUGH, acc=ACC, calcregion=CALCREGION, docalcregion=DOCALCREGION))
+	return (profitMakeModel(model, magzero=magzero, dim=dim, rough=rough, acc=acc, calcregion=calcregion, docalcregion=docalcregion))
 }
-
-#TODO: move CALCREGION to the end and add default matrix.
