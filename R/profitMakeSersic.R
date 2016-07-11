2622e487e79ffb5d3b312cd1314deae7112c2f6a
Aaron Robotham <aaron.robotham@uwa.edu.au>
2016-07-11 17:38:53 +0800

    Cleaned up and simplified profitMakeSersic

--- R/profitMakeSersic.R
+++ R/profitMakeSersic.R
@@ -1,9 +1,9 @@
-profitMakeSersic <- function(xcen = 0, ycen = 0, mag = 15, re = 1, nser = 1, ang = 0, axrat = 1, box = 0, magzero = 0, rough = FALSE, xlim = as.numeric( c(-100,100)), ylim = as.numeric( c(-100,100)), dim = as.integer( c(200,200)), acc = 0.1, docalcregion = FALSE, calcregion) {
+profitMakeSersic <- function(xcen = dim[1]/2, ycen = dim[2]/2, mag = 15, re = 1, nser = 1, ang = 0, axrat = 1, box = 0, magzero = 0, rough = FALSE, dim = c(200,200), acc = 0.1, docalcregion = FALSE, calcregion) {
 	# Simply prepare a model with one sersic profile and fire
 	model = list(
 		sersic = list(
-			xcen = xcen+(xlim[2]-ylim[1])/2,
-			ycen=ycen+(xlim[2]-ylim[1])/2,
+			xcen = xcen,
+			ycen=ycen,
 			mag=mag,
 			re=re,
 			nser=nser,
