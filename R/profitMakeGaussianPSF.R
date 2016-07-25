9e76a3a4dedf97cb1ecfd497d9c7d61edc18336b
taranu <dan.s.taranu@gmail.com>
2016-07-25 18:00:24 +0800

    Rename profitGenPSF -> profitMakeGaussianPSF and have it call profitMakePointSource(); updated docs

--- R/profitMakeGaussianPSF.R
+++ R/profitMakeGaussianPSF.R
@@ -1,9 +1,6 @@
-profitGenPSF=function(FWHM=3, npix=25, sigma){
-  if(missing(sigma)){sigma=FWHM/(2*sqrt(2*log(2)))}
-  x0=y0=npix/2+0.5
-  y=matrix(1:npix,npix,npix)
-  x=t(y)
-  psfanal=exp(-(((x - x0)^2/(2 * sigma^2)) + ((y - y0)^2/(2 * sigma^2))))
-  psfanal=psfanal/sum(psfanal)
-  return(psfanal)
+profitMakeGaussianPSF=function(FWHM=3,npix=25){
+  return(profitMakePointSource(
+    mag=0, model=list(sersic=list(
+      mag=0,re=FWHM,nser=0.5,axrat=1,ang=0)), 
+    image=matrix(0,npix,npix)))
 }
