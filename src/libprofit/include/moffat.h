f0a60a0ba7bc084bc32a88ac1588bc3a64148256
Aaron Robotham <aaron.robotham@uwa.edu.au>
2016-08-03 20:18:33 +0800

    Oops, meant to add teh new Moffat stuff. haven't got far with the C++ code though.

---
+++ src/libprofit/include/moffat.h
@@ -0,0 +1,65 @@
+/**
+ * Header file for sersic profile implementation
+ *
+ * ICRAR - International Centre for Radio Astronomy Research
+ * (c) UWA - The University of Western Australia, 2016
+ * Copyright by UWA (in the framework of the ICRAR)
+ * All rights reserved
+ *
+ * Contributed by Rodrigo Tobar
+ *
+ * This file is part of libprofit.
+ *
+ * libprofit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libprofit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libprofit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _MOFFAT_H_
+#define _MOFFAT_H_
+
+#include "profit.h"
+
+namespace profit
+{
+
+class MoffatProfile : public Profile {
+
+public:
+
+	MoffatProfile();
+
+	void validate();
+	void evaluate(double *image);
+
+	/* General parameters */
+	double xcen;
+	double ycen;
+	double mag;
+	double fwhm;
+	double con;
+
+	/* Used to control the subsampling */
+	bool rough;
+	double acc;
+	double re_switch;
+	unsigned int resolution;
+	unsigned int max_recursions;
+	bool adjust;
+
+	/* Used to avoid outer regions */
+	double re_max;
+
+};
+
+} /* namespace profit */
+
+#endif /* _MOFFAT_H_ */
