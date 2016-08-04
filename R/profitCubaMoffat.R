a8c9797d3671a11dcd1f42af596357eb5540ebb6
Aaron Robotham <aaron.robotham@uwa.edu.au>
2016-08-04 23:46:59 +0800

    Improved profitCubaMoffat and added .Rd file.
    Added box option to profitCubaSersic

--- R/profitCubaMoffat.R
+++ R/profitCubaMoffat.R
@@ -1,36 +1,42 @@
-.profitMoffatScale=function(mag=15, con=2, fwhm=3){
+.profitMoffatScale=function(mag=15, con=2, fwhm=3, axrat=1, box=0){
+  if(box!=0){Rbox=pi*(box+2)/(4*beta(1/(box+2),1+1/(box+2)))}else{Rbox=1}
   rd = fwhm/(2*sqrt(2^(1/con)-1))
-  lumtot = (pi*rd^2)/(con-1)
+  lumtot = (pi*rd^2)*axrat/(con-1)/Rbox
   magtot = -2.5 * log10(lumtot)
   return(1/(10^(0.4 * (mag - magtot))))
 }
 
 
-.profitMoffat=function(r=1, mag=15, con=2, fwhm=3, rd){
+.profitMoffat=function(r=1, mag=15, con=2, fwhm=3, axrat=1, box=0, rd){
   rd = fwhm/(2*sqrt(2^(1/con)-1))
-  Ie=.profitMoffatScale(mag=mag, con=con, fwhm=fwhm)
-  intenr = Ie/((1+(r/rd)^2)^con)
+  intenr = 1/((1+(r/rd)^2)^con)
   return(intenr)
 }
 
-.profitMoffatXY=function(args=c(0,0), xcen=0, ycen=0, mag=15, con=2, fwhm=3){
+.profitMoffatXY=function(args=c(0,0), xcen=0, ycen=0, mag=15, con=2, fwhm=3, ang=0, axrat=1, box=0){
   rad=sqrt((args[1]-xcen)^2+(args[2]-ycen)^2);
-  output=.profitMoffat(rad,mag=mag, con=con, fwhm=fwhm)
+  angrad=-ang*pi/180
+  angmod=atan2((args[1]-xcen),(args[2]-ycen))-angrad;
+  xmod=rad*sin(angmod);
+  ymod=rad*cos(angmod);
+  xmod=xmod/axrat;
+  radmod=(abs(xmod)^(2+box)+abs(ymod)^(2+box))^(1/(2+box))
+  output=.profitMoffat(radmod,mag=mag, con=con, fwhm=fwhm, axrat=axrat, box=box)
   return(output)
 }
 
-.profitMoffatExactSumPix=function(xpix=c(0,1), ypix=c(0,1), xcen=0, ycen=0, mag=15, con=2, fwhm=3, acc=1e-3){
-return(cuhre(2, 1, .profitMoffatXY, xcen=xcen, ycen=ycen, mag=mag, con=con, fwhm=fwhm, rel.tol= acc, abs.tol= 0, lower=c(xpix[1],ypix[1]), upper=c(xpix[2],ypix[2]), flags= list(verbose=0))$value)
+.profitMoffatExactSumPix=function(xpix=c(0,1), ypix=c(0,1), xcen=0, ycen=0, mag=15, con=2, fwhm=3, ang=0, axrat=1, box=0, acc=1e-3){
+return(cuhre(2, 1, .profitMoffatXY, xcen=xcen, ycen=ycen, mag=mag, con=con, fwhm=fwhm, ang=ang, axrat=axrat, box=box, rel.tol= acc, abs.tol= 0, lower=c(xpix[1],ypix[1]), upper=c(xpix[2],ypix[2]), flags= list(verbose=0))$value)
 }
 
-profitCubaMoffat=function(xcen=dim[1]/2, ycen=dim[2]/2, mag=15, con=2, fwhm=3, dim=c(25,25), acc=1e-3){
+profitCubaMoffat=function(xcen=dim[1]/2, ycen=dim[2]/2, mag=15, con=2, fwhm=3, ang=0, axrat=1, box=0, dim=c(25,25), acc=1e-3){
   if(length(dim)==1){dim=rep(dim,2)}
   xpix=0:(dim[1]-1)
   ypix=0:(dim[2]-1)
   pixgrid=expand.grid(xpix,ypix)
   pixval={}
   for(i in 1:length(pixgrid[,1])){
-    pixval=c(pixval, .profitMoffatExactSumPix(c(pixgrid[i,1],pixgrid[i,1]+1), c(pixgrid[i,2],pixgrid[i,2]+1), xcen=xcen, ycen=ycen, mag=mag, con=con, fwhm=fwhm, acc=acc))
+    pixval=c(pixval, .profitMoffatExactSumPix(c(pixgrid[i,1],pixgrid[i,1]+1), c(pixgrid[i,2],pixgrid[i,2]+1), xcen=xcen, ycen=ycen, mag=mag, con=con, fwhm=fwhm, ang=ang, axrat=axrat, box=box, acc=acc))
   }
-  return=matrix(pixval,dim[1],dim[2])
+  return=matrix(pixval*.profitMoffatScale(mag=mag, con=con, fwhm=fwhm, axrat=axrat, box=box),dim[1],dim[2])
 }
--- R/profitCubaSersic.R
+++ R/profitCubaSersic.R
@@ -1,39 +1,40 @@
-.profitSersicScale=function(mag=15, re=1, nser=4, axrat=1, bn= qgamma(0.5, 2 * nser)){
-  lumtot = (re^2)*2*pi*nser*((exp(bn))/(bn^(2*nser)))*gamma(2*nser)*axrat
+.profitSersicScale=function(mag=15, re=1, nser=4, axrat=1, box=0, bn= qgamma(0.5, 2 * nser)){
+  if(box!=0){Rbox=pi*(box+2)/(4*beta(1/(box+2),1+1/(box+2)))}else{Rbox=1}
+  lumtot = (re^2)*2*pi*nser*((exp(bn))/(bn^(2*nser)))*gamma(2*nser)*axrat/Rbox
   magtot = -2.5 * log10(lumtot)
   return(1/(10^(0.4 * (mag - magtot))))
 }
 
 
-.profitSersic=function(r=1, mag=15, re=1, nser=4, axrat=1, bn= qgamma(0.5, 2 * nser)){
-    Ie=.profitSersicScale(mag=mag, re=re, nser=nser, axrat=axrat, bn=bn)
-    intenr = Ie*exp(-bn*(((r/re)^(1/nser)) - 1))
+.profitSersic=function(r=1, mag=15, re=1, nser=4, axrat=1, box=0, bn= qgamma(0.5, 2 * nser)){
+    intenr = exp(-bn*(((r/re)^(1/nser)) - 1))
     return(intenr)
 }
 
 .profitSersicXY=function(args=c(0,0), xcen=0, ycen=0, mag=15, re=1, nser=4, ang=0, axrat=1, box=0, bn= qgamma(0.5, 2 * nser)){
-  rad=sqrt((args[1]-xcen)^2+(args[2]-ycen)^2);
+  rad=sqrt((args[1]-xcen)^2+(args[2]-ycen)^2)
   angrad=-ang*pi/180
-  angmod=atan2((args[1]-xcen),(args[2]-ycen))-angrad;
-  xmod=rad*sin(angmod);
-  ymod=rad*cos(angmod);
-  xmod=xmod/axrat;
-  radmod=(xmod^(2+box)+ymod^(2+box))^(1/(2+box))
-  output=.profitSersic(radmod,mag=mag, re=re, nser=nser, axrat=axrat, bn=bn)
+  angmod=atan2((args[1]-xcen),(args[2]-ycen))-angrad
+  xmod=rad*sin(angmod)
+  ymod=rad*cos(angmod)
+  xmod=xmod/axrat
+  radmod=(abs(xmod)^(2+box)+abs(ymod)^(2+box))^(1/(2+box))
+  output=.profitSersic(radmod, mag=mag, re=re, nser=nser, axrat=axrat, box=box, bn=bn)
   return(output)
 }
 
-.profitSersicExactSumPix=function(xpix=c(0,1), ypix=c(0,1), xcen=0, ycen=0, mag=15, re=1, nser=4, ang=0, axrat=1, box=0, acc=1e-3){
-return(cuhre(2, 1, .profitSersicXY, xcen=xcen, ycen=ycen, mag=mag, nser=nser, re=re, ang=ang, axrat=axrat, bn=qgamma(0.5, 2*nser), rel.tol= acc, abs.tol= 0, lower=c(xpix[1],ypix[1]), upper=c(xpix[2],ypix[2]), flags= list(verbose=0))$value)
+.profitSersicExactSumPix=function(xpix=c(0,1), ypix=c(0,1), xcen=0, ycen=0, mag=15, re=1, nser=4, ang=0, axrat=1, box=0, acc=1e-3, bn= qgamma(0.5, 2 * nser)){
+return(cuhre(2, 1, .profitSersicXY, xcen=xcen, ycen=ycen, mag=mag, nser=nser, re=re, ang=ang, axrat=axrat, box=box, bn=bn, rel.tol= acc, abs.tol= 0, lower=c(xpix[1],ypix[1]), upper=c(xpix[2],ypix[2]), flags= list(verbose=0))$value)
 }
 
 profitCubaSersic=function(xcen=dim[1]/2, ycen=dim[2]/2, mag=15, re=1, nser=4, ang=0, axrat=1, box=0, dim=c(25,25), acc=1e-3){
+  bn= qgamma(0.5, 2 * nser)
   xpix=0:(dim[1]-1)
   ypix=0:(dim[2]-1)
   pixgrid=expand.grid(xpix,ypix)
   pixval={}
   for(i in 1:length(pixgrid[,1])){
-    pixval=c(pixval, .profitSersicExactSumPix(c(pixgrid[i,1],pixgrid[i,1]+1), c(pixgrid[i,2],pixgrid[i,2]+1), xcen=xcen, ycen=ycen, mag=mag, re=re, nser=nser, ang=ang, axrat=axrat, box=box, acc=acc))
+    pixval=c(pixval, .profitSersicExactSumPix(c(pixgrid[i,1],pixgrid[i,1]+1), c(pixgrid[i,2],pixgrid[i,2]+1), xcen=xcen, ycen=ycen, mag=mag, re=re, nser=nser, ang=ang, axrat=axrat, box=box, acc=acc, bn=bn))
   }
-  return=matrix(pixval,dim[1],dim[2])
+  return=matrix(pixval*.profitSersicScale(mag=mag, re=re, nser=nser, axrat=axrat, box=box, bn=bn),dim[1],dim[2])
 }
