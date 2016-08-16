48026308d6f243decb8ed7af751461fbad4850a6
Aaron Robotham <aaron.robotham@uwa.edu.au>
2016-08-16 15:23:52 +0800

    Made some doc changes and removed profitMakeSersic (seems silly to have this for historic reasons, and we don't have equivilants for Moffat and Ferrer).

--- R/profitMakeGaussianPSF.R
+++ R/profitMakeGaussianPSF.R
@@ -1,4 +1,4 @@
 profitMakeGaussianPSF=function(fwhm=3, dim=c(25,25)){
   if(length(dim)==1){dim=rep(dim,2)}
-  return(profitMakeSersic(mag=0, re=fwhm, nser=0.5, axrat=1, ang=0, dim=dim)$z)
+  return(profitMakeModel(list(sersic=list(xcen=dim[1]/2, ycen=dim[2]/2, mag=0, re=fwhm, nser=0.5, axrat=1, ang=0)), dim=dim)$z)
 }
