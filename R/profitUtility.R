4fee24ec084af8eea18dff23c6ce17d72f3be0ed
Aaron Robotham <aaron.robotham@uwa.edu.au>
2017-05-05 22:46:47 +0800

    Minro fixed and more vignette text.

--- R/profitUtility.R
+++ R/profitUtility.R
@@ -211,7 +211,7 @@ profitMakeSegimExpand=function(image, segim, mask=0, objects=0, skycut=1, sigma=
     skyRMS=profitSkyEst(image=profitImDiff(image_sky,3), mask=mask, objects=objects, plot=FALSE)$skyRMS
   }
   if(stats){
-    segstats=profitSegimStats(image=image_orig, segim=segim, sky=sky)
+    segstats=profitSegimStats(image=image_orig, segim=segim_new, sky=sky)
   }else{
     segstats=NULL
   }
