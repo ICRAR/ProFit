caa14104f50e929b0b78c6f3e913018ef4fde58a
Aaron Robotham <aaron.robotham@uwa.edu.au>
2017-05-15 15:25:42 +0800

    Cutout functions moved to magicaxis (see magcutout), and new WCS solving helper functions added there too (more general than just ProFit, so decided to host it in that package).

--- R/profitUtility.R
+++ R/profitUtility.R
@@ -614,25 +614,3 @@ profitSegimPlot=function(image, segim, mask=0, sky=0, ...){
     magimage(mask, lo=0, hi=1, col=c(NA,hsv(alpha=0.3)), add=T)
   }
 }
-
-profitCutout=function(image, loc=dim(image)/2, box=c(101,101), plot=FALSE, ...){
-  loc=as.numeric(loc)
-  xcen=loc[1]
-  ycen=loc[2]
-  loc=ceiling(loc)
-  xlo=ceiling(loc[1]-(box[1]/2-0.5))
-  xhi=ceiling(loc[1]+(box[1]/2-0.5))
-  ylo=ceiling(loc[2]-(box[2]/2-0.5))
-  yhi=ceiling(loc[2]+(box[2]/2-0.5))
-  if(xlo<1){xlo=1}
-  if(xhi>dim(image)[1]){xhi=dim(image)[1]}
-  if(ylo<1){ylo=1}
-  if(yhi>dim(image)[2]){yhi=dim(image)[2]}
-  cutim=image[xlo:xhi, ylo:yhi]
-  output=list(cutim=cutim, loc=c(xcen-xlo+1,ycen-ylo+1), loc.orig=c(xcen,ycen), loc.diff=c(xlo-1,ylo-1), xsel=xlo:xhi, ysel=ylo:yhi)
-  if(plot){
-    magimage(output, ...)
-  }
-  return=output
-  
-}
