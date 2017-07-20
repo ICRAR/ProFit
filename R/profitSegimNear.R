6c4369b29dcb5d1589f304fefab1b304f902139a
Aaron Robotham <aaron.robotham@uwa.edu.au>
2017-07-20 16:34:10 +0800

    Some minor fixes and vignette update.

--- R/profitSegimNear.R
+++ R/profitSegimNear.R
@@ -32,5 +32,7 @@ profitSegimNear=function(segim, offset=1){
   
   #The line means for each set of segID pixels, identify unique adjacent pixels that do not have the ID of the segID of interest or 0 (sky) and sort the touching ID list and return to a listed data.frame. Ouch!
   
-  return=as.data.frame(tabcomb[,list(nearID=list(sort(setdiff(unique(c(down,left,up,right)),c(0,segID))))),by=segID])
+  tabnear=tabcomb[,list(nearID=list(sort(setdiff(unique(c(down,left,up,right)),c(0,segID))))),by=segID]
+  tabnear[,Nnear:=length(unlist(nearID)),by=segID]
+  return=tabnear
 }
