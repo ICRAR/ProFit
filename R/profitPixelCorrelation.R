e2e9e047490a585d744ff15e3e5011c1eb047732
Aaron Robotham <aaron.robotham@uwa.edu.au>
2017-07-21 17:26:25 +0800

    Add option to not return FFT (for speed and memory reasons, you might not want to run it).

--- R/profitPixelCorrelation.R
+++ R/profitPixelCorrelation.R
@@ -1,4 +1,4 @@
-profitPixelCorrelation=function(image, objects, mask, sky=0, skyRMS=1, offset=c(1:9,1:9*10,1:9*100,1:9*1e3,1:9*1e4), plot=FALSE){
+profitPixelCorrelation=function(image, objects, mask, sky=0, skyRMS=1, offset=c(1:9,1:9*10,1:9*100,1:9*1e3,1:9*1e4), fft=TRUE, plot=FALSE){
   
   xlen=dim(image)[1]
   ylen=dim(image)[2]
@@ -22,26 +22,30 @@ profitPixelCorrelation=function(image, objects, mask, sky=0, skyRMS=1, offset=c(
     cory=c(cory,cor(as.numeric(image[,1:(ylen-i)]), as.numeric(image[,1:(ylen-i)+i]), use="complete.obs"))
   }
   
-  output=cbind(offset=offset,corx=corx,cory=cory)
+  output_cortab=cbind(offset=offset,corx=corx,cory=cory)
   
-  xlenpad=xlen+xlen%%2
-  ylenpad=ylen+ylen%%2
-  
-  if(!missing(objects)){
-    image[objects==1]=rnorm(length(which(objects==1)))
+  if(fft){
+    xlenpad=xlen+xlen%%2
+    ylenpad=ylen+ylen%%2
+    
+    if(!missing(objects)){
+      image[objects==1]=rnorm(length(which(objects==1)))
+    }
+    
+    if(!missing(mask)){
+      image[mask==1]==rnorm(length(which(mask==1)))
+    }
+    
+    centre=matrix(c(rep(c(-1,1),xlenpad/2), rep(c(1,-1),ylenpad/2)),xlenpad,ylenpad)[1:xlen,1:ylen]
+    output_FFT=Mod(fft(z=image*centre))
+  }else{
+    output_cortab=NULL
   }
   
-  if(!missing(mask)){
-    image[mask==1]==rnorm(length(which(mask==1)))
-  }
-  
-  centre=matrix(c(rep(c(-1,1),xlenpad/2), rep(c(1,-1),ylenpad/2)),xlenpad,ylenpad)[1:xlen,1:ylen]
-  outputFFT=Mod(fft(z=image*centre))
-
   if(plot){
-    magplot(output[,c(1,2)], xlim=c(1,max(offset)), ylim=c(-1,1), type='l', col='blue', log='x', grid=TRUE)
-    lines(output[,c(1,3)], col='red')
+    magplot(output_cortab[,c(1,2)], xlim=c(1,max(offset)), ylim=c(-1,1), type='l', col='blue', log='x', grid=TRUE)
+    lines(output_cortab[,c(1,3)], col='red')
   }
   
-  return=list(cortab=output, fft=outputFFT)
+  return=list(cortab=output_cortab, fft=output_FFT)
 }
