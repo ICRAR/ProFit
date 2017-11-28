0331ca9eb0ac4990136598b679c890fb64447314
taranu <dan.s.taranu@gmail.com>
2017-11-28 11:25:30 +0800

    Wrong sign for the mask. Oops.

--- src/libprofit/src/convolve.cpp
+++ src/libprofit/src/convolve.cpp
@@ -76,7 +76,7 @@ Image BruteForceConvolver::convolve(const Image &src, const Image &krn, const Ma
 			auto im_idx = i + j * src_width;
 
 			/* Don't convolve this pixel */
-			if( !mask.empty() and mask_data[im_idx]) {
+			if( !mask.empty() and !mask_data[im_idx]) {
 				out[im_idx] = 0;
 				continue;
 			}
